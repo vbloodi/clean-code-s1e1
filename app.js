2341e9c42bca3371711508ed11e442a08194680f
vbloodi <bloodi.code@gmail.com>
2023-04-24 16:37:11 +0300

    fix(basic-3.4): add tag selectors

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ var completedTasksHolder = document.getElementById('completed-tasks'); //complet
 //New task list item
 var createNewTaskElement = function (taskString) {
   var listItem = document.createElement('li');
+  listItem.classList.add('list__item');
 
   //input (checkbox)
   var checkBox = document.createElement('input'); //checkbx
@@ -77,7 +78,7 @@ var editTask = function () {
   var editInput = listItem.querySelector('input[type=text]');
   var label = listItem.querySelector('label');
   var editBtn = listItem.querySelector('.edit');
-  var containsClass = listItem.classList.contains('editMode');
+  var containsClass = listItem.classList.contains('edit-mode');
   //If class of the parent is .editmode
   if (containsClass) {
     //switch to .editmode
@@ -90,7 +91,7 @@ var editTask = function () {
   }
 
   //toggle .editmode on the parent.
-  listItem.classList.toggle('editMode');
+  listItem.classList.toggle('edit-mode');
 };
 
 //Delete task.
